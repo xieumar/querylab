e4aa4327cd928996b59bfb31020f287e8a08efb1
Zainab Umar Ibrahim <xieumar@gmail.com>
2026-06-02 02:06:13 +0100

    feat(external-pages): implement templates, docs, and faq pages

---
+++ app/(external)/faq/page.tsx
@@ -0,0 +1,5 @@
+import { FAQPage } from "@/components/pages";
+
+export default function Page() {
+  return <FAQPage />;
+}
--- app/(external)/layout.tsx
+++ app/(external)/layout.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { Navbar } from "../../components/external-pages/Navbar";
-import { Footer } from "../../components/external-pages/Footer";
+import { Navbar } from "@/components/external-pages/Navbar";
+import { Footer } from "@/components/external-pages/Footer";
 
 export default function ExternalLayout({
   children,
